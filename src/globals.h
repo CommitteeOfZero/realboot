3bb762def18cdb67e5eda77b12c0a60f27cc72f8
Enorovan <enorovan@gmail.com>
2024-09-10 20:56:36 +0200

    C;C GOG

--- src/globals.h
+++ src/globals.h
@@ -162,19 +162,21 @@ const QString game_Custom2ButtonLabel = "Delusion trigger (Right)";
 const QString game_ReleaseUrl =
     "https://github.com/CommitteeOfZero/cc-patch/releases";
 #if defined(GAME_STEAM)
-const QString game_LauncherTitle = "CHAOS;CHILD Steam Patch Launcher";
+const QString game_LauncherTitle =
+    "CHAOS;CHILD Improvement Patch (Steam) Launcher";
 const QString game_PatchConfPath = "Committee of Zero/CCSteam";
 const QString game_GameConfPath = "mages_steam/CHAOS CHILD/eng";
 const QString game_LaunchCommand = "Game.exe chaoschild EN";
-#else
-const QString game_LauncherTitle = "CHAOS;CHILD English Launcher";
-const QString game_PatchConfPath = "Committee of Zero/CCGOG";
-const QString game_GameConfPath = "Mages Inc/CHAOS CHILD";
-const QString game_LaunchCommand = "Game_GOG.exe chaoschild";
 #define IPC_ENABLED
 const uint32_t game_ipcIn = 0x23F4F1DE;
 const uint32_t game_ipcOut = 0xF0973746;
 const wchar_t game_ipcName[] = L"CHAOS;CHILD";
+#else
+const QString game_LauncherTitle =
+    "CHAOS;CHILD Improvement Patch (GOG) Launcher";
+const QString game_PatchConfPath = "Committee of Zero/CCGOG";
+const QString game_GameConfPath = "Mages Inc/CHAOS;CHILD/eng";
+const QString game_LaunchCommand = "Game.exe chaoschild EN";
 #endif
 #endif
 
